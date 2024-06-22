6965ddf6cf7963ea065361198c1ecf5f493501bd
William Mckee <williamcmckee@protonmail.com>
2024-06-23 00:12:38 +1200

    remove hell page, add default to join and report

---
+++ src/app/join/page.tsx
@@ -0,0 +1,18 @@
+import NavBar from './_components/navbar';
+ 
+export default async function Page() { 
+	try {
+
+			return (
+			 <main className="bg-cyan-900 min-h-screen text-white">
+			<NavBar />
+			 <div className="container mx-auto p-6">
+			  <p className="text-lg mb-6">WELCOME TO SUPER EARTH WEBSITE!</p>
+			  </div>
+			  </main>		 
+	);
+	} catch (error) {
+		console.error('Error fetching data', error);
+		return <main>Error fetching data. Please try again later.</main>;
+	}
+}
